c11ee469ced39bdd19364aad83dbc9454e6a1bf0
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-02-22 23:36:05 +0900

    feat: beanUtils toDto, toEntity

---
+++ product-service/src/main/java/co/whitetree/productservice/util/EntityDtoUtil.java
@@ -0,0 +1,20 @@
+package co.whitetree.productservice.util;
+
+import co.whitetree.productservice.dto.ProductDto;
+import co.whitetree.productservice.entity.Product;
+import org.springframework.beans.BeanUtils;
+
+public class EntityDtoUtil {
+
+    public static ProductDto toDto(Product product) {
+        ProductDto dto = new ProductDto();
+        BeanUtils.copyProperties(product, dto);
+        return dto;
+    }
+
+    public static Product toEntity(ProductDto productDto) {
+        Product product = new Product();
+        BeanUtils.copyProperties(productDto, product);
+        return product;
+    }
+}
