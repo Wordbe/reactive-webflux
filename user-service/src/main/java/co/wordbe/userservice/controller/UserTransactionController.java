cea55598d8e7d68dd40266af42119597da59fa0d
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-02-26 01:45:22 +0900

    feat: user transaction

---
+++ user-service/src/main/java/co/wordbe/userservice/controller/UserTransactionController.java
@@ -0,0 +1,24 @@
+package co.wordbe.userservice.controller;
+
+import co.wordbe.userservice.dto.TransactionRequestDto;
+import co.wordbe.userservice.dto.TransactionResponseDto;
+import co.wordbe.userservice.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("user/transaction")
+@RequiredArgsConstructor
+public class UserTransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public Mono<TransactionResponseDto> createTransaction(@RequestBody Mono<TransactionRequestDto> requestDtoMono) {
+        return requestDtoMono.flatMap(transactionService::createTransaction);
+    }
+}
