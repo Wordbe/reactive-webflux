f57fa1d8b06fd27303142d7aaf5f7b92dd4101c8
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-02-25 18:26:54 +0900

    feat: transaction service

---
+++ user-service/src/main/java/co/wordbe/userservice/service/TransactionService.java
@@ -0,0 +1,27 @@
+package co.wordbe.userservice.service;
+
+import co.wordbe.userservice.dto.TransactionRequestDto;
+import co.wordbe.userservice.dto.TransactionResponseDto;
+import co.wordbe.userservice.dto.TransactionStatus;
+import co.wordbe.userservice.repository.UserRepository;
+import co.wordbe.userservice.repository.UserTransactionRepository;
+import co.wordbe.userservice.util.EntityDtoUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+    private final UserRepository userRepository;
+    private final UserTransactionRepository userTransactionRepository;
+
+    public Mono<TransactionResponseDto> createTransaction(final TransactionRequestDto requestDto) {
+        return userRepository.updateUserBalance(requestDto.getUserId(), requestDto.getAmount())
+                .filter(Boolean::booleanValue)
+                .map(b -> EntityDtoUtil.toEntity(requestDto))
+                .flatMap(userTransactionRepository::save)
+                .map(ut -> EntityDtoUtil.toDto(requestDto, TransactionStatus.APPROVED))
+                .defaultIfEmpty(EntityDtoUtil.toDto(requestDto, TransactionStatus.DECLINED));
+    }
+}
--- user-service/src/main/java/co/wordbe/userservice/util/EntityDtoUtil.java
+++ user-service/src/main/java/co/wordbe/userservice/util/EntityDtoUtil.java
@@ -1,8 +1,14 @@
 package co.wordbe.userservice.util;
 
+import co.wordbe.userservice.dto.TransactionRequestDto;
+import co.wordbe.userservice.dto.TransactionResponseDto;
+import co.wordbe.userservice.dto.TransactionStatus;
 import co.wordbe.userservice.dto.UserDto;
 import co.wordbe.userservice.entity.User;
+import co.wordbe.userservice.entity.UserTransaction;
 import org.springframework.beans.BeanUtils;
+
+import java.time.LocalDateTime;
 
 public class EntityDtoUtil {
 
@@ -16,5 +22,21 @@ public class EntityDtoUtil {
         User user = new User();
         BeanUtils.copyProperties(dto, user);
         return user;
+    }
+
+    public static UserTransaction toEntity(TransactionRequestDto requestDto) {
+        UserTransaction userTransaction = new UserTransaction();
+        userTransaction.setUserId(requestDto.getUserId());
+        userTransaction.setAmount(requestDto.getAmount());
+        userTransaction.setTransactionDate(LocalDateTime.now());
+        return userTransaction;
+    }
+
+    public static TransactionResponseDto toDto(TransactionRequestDto requestDto, TransactionStatus status) {
+        TransactionResponseDto responseDto = new TransactionResponseDto();
+        responseDto.setAmount(requestDto.getAmount());
+        responseDto.setUserId(requestDto.getUserId());
+        responseDto.setStatus(status);
+        return responseDto;
     }
 }
