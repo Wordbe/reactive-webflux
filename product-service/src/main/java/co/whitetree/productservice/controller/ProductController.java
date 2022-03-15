3faaef0bdefc8fdf4a74e31452f3a91ae90a2eb0
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-03-15 18:07:12 +0900

    feat: reactive order service resilient

--- product-service/src/main/java/co/whitetree/productservice/controller/ProductController.java
+++ product-service/src/main/java/co/whitetree/productservice/controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 @RequestMapping("product")
 @RequiredArgsConstructor
@@ -27,6 +29,7 @@ public class ProductController {
 
     @GetMapping("{id}")
     public Mono<ResponseEntity<ProductDto>> getProductById(@PathVariable String id) {
+        simulateRandomException();
         return productService.getProductById(id)
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
@@ -48,5 +51,12 @@ public class ProductController {
     @DeleteMapping("{id}")
     public Mono<Void> deleteProduct(@PathVariable String id) {
         return productService.deleteProduct(id);
+    }
+
+    // Make exceptions intentionally
+    private void simulateRandomException() {
+        int nextInt = ThreadLocalRandom.current().nextInt(1, 10);
+        if (nextInt > 5)
+            throw new RuntimeException("Something is wrong");
     }
 }
